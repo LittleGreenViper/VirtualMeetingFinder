672f20ded52b8a509e7214c8439ca950ee619843
Chris Marshall <chris@riftvalleysoftware.com>
2024-06-21 16:15:06 -0400

    Initial Commit

---
+++ VirtualMeetingFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirtualMeetingFinder
+//
+//  Created by Chris Marshall on 6/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

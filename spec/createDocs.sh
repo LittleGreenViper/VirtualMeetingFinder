7b2b042129a5a60857193030938a8a62474261f4
Chris Marshall <chris@riftvalleysoftware.com>
2024-08-01 14:26:23 -0400

    Yeaaahhh... I'm deleting the docs directory for now. The whole export docc for GH pages thing isn't working properly. I'm sure it's me, but I need to find out what I'm doing wrong.

---
+++ spec/createDocs.sh
@@ -0,0 +1,4 @@
+$(xcrun --find docc) process-archive \
+transform-for-static-hosting "/Users/chrismarshall/Developer/LGV/VirtualMeetingFinder/VMF.doccarchive" \
+--output-path docs \
+--hosting-base-path "https://littlegreenviper.github.io/VirtualMeetingFinder/"
